a4547c4f05bab98e3ef2c69fc4e76fe5ab41e3fa
Haksoo Kim <profhskim2@gmail.com>
2022-02-04 13:38:54 +0900

    2022-02-04 13:38

--- gif-chat/app.js
+++ gif-chat/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const nunjucks = require('nunjucks')
 const dotenv = require('dotenv')
+const ColorHash = require('color-hash')
 
 dotenv.config()
 const webSocket = require('./socket')
@@ -21,20 +22,30 @@ nunjucks.configure('views',{
 
 connect()
 
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(session({
-    resave:false,
+const sessionMiddleware = session({
+    resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
         httpOnly: true,
         secure: false,
     }
-}))
+})
+
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(sessionMiddleware)
+
+app.use((req,res,next)=>{
+    if (!req.session.color){
+        const colorHash = new ColorHash()
+        req.session.color = colorHash.hex(req.sessionID)
+    }
+    next()
+})
 
 app.use('/',indexRouter)
 
@@ -55,4 +66,4 @@ const server = app.listen(app.get('port'),()=>{
     console.log(app.get('port'),'번 포트에서 대기 중')
 })
 
-webSocket(server)
+webSocket(server,app,sessionMiddleware)
--- gif-chat/socket.js
+++ gif-chat/socket.js
@@ -24,7 +24,7 @@
 //     })
 // }
 
-const SocketIO = require('socket.io')
+// const SocketIO = require('socket.io')
 
 // module.exports = (server) => {
 //     const io = SocketIO(server, {path:'/socket.io'})
@@ -50,12 +50,17 @@ const SocketIO = require('socket.io')
 // }
 
 const SocketIO = require('socket.io')
+const axios = require('axios')
 
-module.exports = (server, app) => {
+module.exports = (server, app, sessionMiddleware) => {
     const io = SocketIO(server, {path:'/socket.io'})
     app.set('io',io)
     const room = io.of('/room')
     const chat = io.of('/chat')
+
+    io.use((socket,next)=>{
+        sessionMiddleware(socket.request, socket.request.res, next)
+    })
 
     room.on('connection', (socket) => {
         console.log('room 네임스페이스에 접속')
@@ -72,10 +77,30 @@ module.exports = (server, app) => {
         .split('/')[referer.split('/').length-1]
         .replace(/\?.+/,'')
         socket.join(roomId)
+        socket.to(roomId).emit('join',{
+            user:'system',
+            chat: `${req.session.color}님이 입장하셨습니다.`,
+        })
 
         socket.on('disconnect',()=>{
             console.log('chat 네임스페이스 접속 해제')
             socket.leave(roomId)
+            const currentRoom = socket.adapter.rooms[roomId]
+            const userCount = currentRoom? currentRoom.length : 0
+            if (userCount === 0) {
+                axios.delete(`http://localhost:8005/room/${roomId}`)
+                .then(()=>{
+                    console.log('방 제거 요청 성공')
+                })
+                .catch((error)=>{
+                    console.error(error)
+                })
+            } else {
+                socket.to(roomId).emit('exit',{
+                    user:'system',
+                    chat: `${req.session.color}님이 퇴장하셨습니다.`
+                })
+            }
         })
     })
 }
